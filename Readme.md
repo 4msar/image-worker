7e9ebfbe6849c1d6484a71da4b6e985cf685f2e3
Saiful Alam Rakib <msa4rakib@gmail.com>
2025-02-25 01:38:07 +0600

    Added new image worker

---
+++ Readme.md
@@ -0,0 +1,35 @@
+# Unsplash Worker
+
+This worker is responsible for fetching images from the Unsplash API.
+
+## Configuration
+
+The worker requires the following environment variables:
+
+- `UNSPLASH_ACCESS_KEY`: The access key for the Unsplash API.
+
+use the parameters from [here](https://unsplash.com/documentation#get-a-random-photo)
+
+## Installation
+
+To install the worker, run the following command:
+
+```bash
+yarn install
+```
+
+## Running the Worker
+
+To run the worker, use the following command:
+
+```bash
+yarn dev
+```
+
+## Deploy
+
+To deploy the worker, run the following command:
+
+```bash
+yarn deploy
+```
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import docs from './docs.html';
+
+/**
+ * @typedef {Object} Env
+ */
+
+export default {
+	/**
+	 * @param {Request} request
+	 * @param {Env & Record<string, string>} env
+	 * @param {ExecutionContext} ctx
+	 * @returns {Promise<Response>}
+	 */
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const preferredResponseType = url.searchParams.get('type');
+		url.searchParams.delete('type');
+
+		if (request.method !== 'GET') {
+			return new Response('Method Not Allowed', {
+				status: 405,
+				headers: {
+					'content-type': 'text/plain',
+				},
+			});
+		}
+
+		if (url.pathname.startsWith('/unsplash/') && checkCorrectUnsplashEndpoint(url.pathname)) {
+			let path = url.pathname;
+
+			// Remove the /unsplash/ prefix from start of path
+			path = path.replace('/unsplash/', '');
+
+			url.searchParams.set('client_id', env.UNSPLASH_ACCESS_KEY);
+
+			return getUnsplashImage(path, url.searchParams, preferredResponseType);
+		}
+
+		return new Response(docs, {
+			headers: {
+				'content-type': 'text/html',
+			},
+		});
+	},
+};
+
+const getUnsplashImage = async (path: string, params: URLSearchParams, type: 'image' | 'json' | 'redirect' | 'view') => {
+	const unsplashResponse = await fetch(`https://api.unsplash.com/${path}?${params.toString()}`);
+	const unsplashResult: UnsplashResponse = await unsplashResponse.json();
+
+	if (type === 'json') {
+		return new Response(JSON.stringify(unsplashResult), {
+			headers: {
+				'content-type': 'application/json',
+			},
+		});
+	}
+
+	if (type === 'view') {
+		return Response.redirect(unsplashResult.links.html, 301);
+	}
+
+	if (type === 'redirect') {
+		return Response.redirect(unsplashResult.urls.full, 301);
+	}
+
+	const unsplashImageResponse = await fetch(unsplashResult.urls.regular);
+	const unsplashImageBuffer = await unsplashImageResponse.arrayBuffer();
+	const unsplashImage = new Uint8Array(unsplashImageBuffer);
+
+	return new Response(unsplashImage, {
+		headers: {
+			'content-type': unsplashImageResponse.headers.get('content-type') || 'image/jpeg',
+		},
+	});
+};
+
+const checkCorrectUnsplashEndpoint = (path: string): boolean => {
+	const unsplashEndpoints = ['/unsplash/photos', '/unsplash/collections', '/unsplash/users', '/unsplash/search'];
+
+	return unsplashEndpoints.some((endpoint) => path.startsWith(endpoint));
+};
